881d5040a8d1ceb0f1cc6158637db9586c9f0ccf
lijiancheng <lijiancheng0614@gmail.com>
2014-07-02 23:35:22 +0800

    add ReleaseForm

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 #coding=utf-8
 from flask.ext.wtf import Form
 from wtforms import TextField
+from wtforms import BooleanField
 from wtforms.validators import Required
 
 import sys
@@ -16,3 +17,11 @@ class RegisterForm(Form):
 	password = TextField('Password', validators = [Required()])
 	password_again = TextField('Password again', validators = [Required()])
 	mail = TextField('Mail', validators = [Required()])
+
+class ReleaseForm(Form):
+  start_time = TextField('Start time', validators = [Required()])
+  close_time = TextField('Close time', validators = [Required()])
+  is_allow_anonymous = BooleanField('Allow anonymous')
+  limit_num_participants = TextField('Limit delivery number for participant')
+  limit_num_ip = TextField('Limit delivery number for IP address')
+  special_participants = TextField('Special participants')
