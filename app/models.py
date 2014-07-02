c81fcf164d8b6e98ccc8468e3acd5503bacd41dc
Siyu Yang <yangsiyu1992@gmail.com>
2014-07-02 20:19:28 +0800

    Add model 'Questionnarie'

--- app/models.py
+++ app/models.py
@@ -8,6 +8,8 @@ class User(db.Model):
 	isAdmin = db.Column(db.Boolean)
 	isClosed = db.Column(db.Boolean)
 
+	questionnaires = db.relationship("Questionnaire", backref='user', lazy='dynamic')
+
 	def is_authenticated(self):
 		return True
 
@@ -22,3 +24,12 @@ class User(db.Model):
 
 	def __repr__(self):
 		return '<User %r>' % (self.username)
+
+class Questionnaire(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(100))
+	subject = db.Column(db.String(100))
+	description = db.Column(db.Text)
+	create_time = db.Column(db.DateTime)
+	schema = db.Column(db.PickleType)
+	author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
