1472bb80c0beaf6a0e0d7150b74f3c383c634fd6
lijiancheng <lijiancheng0614@gmail.com>
2014-07-04 16:17:32 +0800

    administrator page: list all users & all questionnaires

--- app/administrator_views.py
+++ app/administrator_views.py
@@ -18,3 +18,43 @@ def administrator(username):
     return render_template('administrator.html',
         users = users,
         questionnaires = questionnaires)
+
+@app.route('/administrator/<string:username>/ban/<int:userid>')
+@login_required
+def ban(username, userid):
+    user = g.user
+    if user == None or user.isAdmin == False:
+      pass
+    else:
+      u = User.query.get(userid)
+      if u == None:
+        flash("No such user")
+      elif u.isClosed == 1:
+        flash("The user has been banned")
+      else:
+        u.isClosed = 1
+        db.session.add(u)
+        db.session.commit()
+        flash("Ban successfully")
+  
+    return redirect(url_for('administrator', username = username))
+
+@app.route('/administrator/<string:username>/unban/<int:userid>')
+@login_required
+def unban(username, userid):
+    user = g.user
+    if user == None or user.isAdmin == False:
+      pass
+    else:
+      u = User.query.get(userid)
+      if u == None:
+        flash("No such user")
+      elif u.isClosed == 0:
+        flash("The user has been unbanned")
+      else:
+        u.isClosed = 0
+        db.session.add(u)
+        db.session.commit()
+        flash("Unban successfully")
+  
+    return redirect(url_for('administrator', username = username))
