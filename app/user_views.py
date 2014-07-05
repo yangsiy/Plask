235f393c616314768a21356ca58179cc594b8c26
Windy Darian <p123456638@msn.com>
2014-07-05 13:58:48 +0800

    Tiny fix

--- app/user_views.py
+++ app/user_views.py
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 #-*- coding:utf-8 -*- 
 
-from flask import request,render_template, g, flash, url_for, redirect
+from flask import request,render_template, g, flash, url_for, redirect, abort
 from flask.ext.login import login_required
 from app import app, db
 from models import User, Questionnaire
@@ -9,36 +8,15 @@ from models import User, Questionnaire
 @app.route('/user/<string:username>')
 @login_required
 def user(username):
-    user = User.query.filter_by(username = username)[0]
+    users = list(User.query.filter_by(username = username))
+    if not users:
+        abort(404)
+    user = users[0]
     created = Questionnaire.query.filter_by(author_id = user.id).all()
-    ques = user.quesanswers
-    quess = []
-    for each in ques:
-        if each.ques_id not in quess:
-            quess.append(each.ques_id)
-    answerd = []
-    for each in quess:
-        answerd.append(Questionnaire.query.get(each))
-    return render_template('user.html',
-            g = g,
-            ques_list_created = created,
-            ques_list_filled = answerd)
-=======
-#-*- coding:utf-8 -*- 
-
-from flask import request,render_template, g, flash, url_for, redirect
-from flask.ext.login import login_required
-from app import app, db
-from models import User, Questionnaire
-
-@app.route('/user/<string:username>')
-@login_required
-def user(username):
-    created = Questionnaire.query.filter_by(author_id = g.user.id).all()
     created = list(created)
     releases = [x.retrive_last_release() for x in created]
     
-    ques_answers = list(g.user.quesanswers)
+    ques_answers = list(user.quesanswers)
     print str(ques_answers)
     '''
     quess = []
@@ -54,4 +32,3 @@ def user(username):
             ques_list_created = created,
             ques_list_releases = releases,
             ques_ans_list = ques_answers)
->>>>>>> refs/heads/fry
