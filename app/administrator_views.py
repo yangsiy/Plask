27b30b70ddf582a96c6c8a46372693ee9d416479
lijiancheng <lijiancheng0614@gmail.com>
2014-07-07 17:19:23 +0800

    update admin - query data in a week

--- app/administrator_views.py
+++ app/administrator_views.py
@@ -3,7 +3,8 @@
 from flask import request,render_template, g, flash, url_for, redirect
 from flask.ext.login import login_required
 from app import app, db
-from models import User, Questionnaire
+from models import User, Questionnaire, QuesAnswer
+from datetime import datetime, timedelta
 
 def is_not_admin():
     user = g.user
@@ -17,12 +18,34 @@ def administrator():
     if is_not_admin():
         pass
     else:
+        d = datetime.now()
+        oneday = timedelta(days=1)
+        questionnaires_week = []
+        answer_week = []
+
+        for i in range(7):
+            date_from = datetime(d.year, d.month, d.day, 0, 0, 0)
+            date_to = datetime(d.year, d.month, d.day, 23, 59, 59)
+            questionnaires_week.append(
+                (d.date(),
+                Questionnaire.query.filter(
+                Questionnaire.create_time >= date_from).filter(
+                Questionnaire.create_time <= date_to).count()))
+            answer_week.append(
+                (d.date(),
+                QuesAnswer.query.filter(
+                QuesAnswer.date >= date_from).filter(
+                QuesAnswer.date <= date_to).count()))
+            d = d - oneday
+
         users = User.query.all()
         questionnaires = Questionnaire.query.all()
 
     return render_template('administrator.html',
         users = users,
-        questionnaires = questionnaires)
+        questionnaires = questionnaires,
+        questionnaires_week = questionnaires_week,
+        answer_week = answer_week)
 
 @app.route('/administrator/ban_user/<int:userid>')
 @login_required
