b5642bf3f0ea7cd009d294150a4ae53009f59869
marco <marcowong1019@hotmail.com>
2014-07-11 10:09:09 +0800

    change the icon to the text which on the navbar

--- add_admin.py
+++ add_admin.py
@@ -5,7 +5,7 @@ user = User(username = 'admin',
 			password = 'admin',
 			mail = 'admin@admin.com',
 			is_admin = True,
-			is_ban = False)
+			is_ban = False,)
 
 db.session.add(user)
 db.session.commit()
